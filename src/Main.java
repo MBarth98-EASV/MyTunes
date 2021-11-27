86da29cf449974d603a0ef4d05ad3ecb6656eae8
sandbxk <90904706+sandbxk@users.noreply.github.com>
2021-11-27 15:25:18 +0100

    Tweaked scenebuilder

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/View/MyTunesView.fxml"));
         primaryStage.setTitle("MyTunes");
-        primaryStage.setScene(new Scene(root, 1050, 600));
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 

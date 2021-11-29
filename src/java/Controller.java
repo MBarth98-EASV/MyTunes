454579354111f0978685345ded548f1fff14a16e
Mads Mandahl-Barth <mads-barth@live.dk>
2021-11-29 18:27:50 +0100

    gui refactor + action definitions

--- src/java/Controller.java
+++ src/java/Controller.java
@@ -1,48 +1,69 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.beans.EventHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable
 {
-    public RadioButton radioBtnYoutube;
-    public RadioButton radioBtnServer;
-    public RadioButton radioBtnLocal;
-    public TextField txtFieldSearch;
-    public Button btnSettings;
+    @FXML public TextField txtFieldSearch;
 
-    public TreeTableView treeTblViewPlaylists;
-    public TreeTableColumn treeTblClmnPlaylist;
-    public TreeTableColumn treeTblClmnPlaylistArtist;
-    public TreeTableColumn treeTblClmnPlaylistSongs;
-    public TreeTableColumn treeTblClmnPlaylistTime;
-    public Button btnPlaylistUp;
-    public Button btnPlaylistDown;
-    public MenuButton mnuBtnPlaylistOptions;
+    @FXML public Button btnSettings;
 
-    public TableView tblViewSongs;
-    public TableColumn tblClmnSongTitle;
-    public TableColumn tblClmnSongArtist;
-    public TableColumn tblClmnSongAlbum;
-    public TableColumn tblClmnSongGenre;
-    public TableColumn tblClmnSongTime;
-    public Button btnSongUp;
-    public Button btnSongDown;
-    public MenuButton mnuBtnSongOptions;
+    @FXML public TreeTableView treeView;
 
-    public Button btnPlayPause;
-    public Button btnNextSong;
-    public Button btnPrevSong;
-    public Label lblSongName;
-    public Label lblSongCurrentTime;
-    public Label lblSongTotalTime;
-    public Slider sliderSong;
-    public ToggleButton tglBtnShuffle;
-    public Slider sliderVolume;
+    @FXML public TreeTableColumn tvColumnPlaylist;
+
+    @FXML public TreeTableColumn tvColumnArtist;
+
+    @FXML public TreeTableColumn tvColumnSongs;
+
+    @FXML public TreeTableColumn tvColumnTime;
+
+    @FXML public Button btnPlaylistUp;
+
+    @FXML public Button btnPlaylistDown;
+
+    @FXML public TableView tblViewSongs;
+
+    @FXML public TableColumn tblClmnSongTitle;
+
+    @FXML public TableColumn tblClmnSongArtist;
+
+    @FXML public TableColumn tblClmnSongAlbum;
+
+    @FXML public TableColumn tblClmnSongGenre;
+
+    @FXML public TableColumn tblClmnSongTime;
+
+    @FXML public Button btnSongUp;
+
+    @FXML public Button btnSongDown;
+
+    @FXML public Button btnPlayPause;
+
+    @FXML public Button btnNextSong;
+
+    @FXML public Button btnPrevSong;
+
+    @FXML public Label lblSongName;
+
+    @FXML public Label lblSongCurrentTime;
+
+    @FXML public Label lblSongTotalTime;
+
+    @FXML public Slider sliderSong;
+
+    @FXML public ToggleButton tglBtnShuffle;
+
+    @FXML public Slider sliderVolume;
 
 
-    public Controller(){
+    public Controller()
+    {
 
     }
 
@@ -52,4 +73,38 @@ public class Controller implements Initializable
 
     }
 
+    @FXML public void onPlayTrack(ActionEvent actionEvent)
+    {
+        System.out.println("play/pause current song");
+    }
+
+    public void onNextTrack(ActionEvent actionEvent)
+    {
+        System.out.println("next song");
+    }
+
+    public void onPreviousTrack(ActionEvent actionEvent)
+    {
+        System.out.println("previous/reset song");
+    }
+
+    public void onRandomTrack(ActionEvent actionEvent)
+    {
+        System.out.println("get a random song");
+    }
+
+    public void onSettings(ActionEvent actionEvent)
+    {
+        System.out.println("open settings");
+    }
+
+    public void onMoveTrackUp(ActionEvent actionEvent)
+    {
+        System.out.println("track is moved upwards");
+    }
+
+    public void onMoveTrackDown(ActionEvent actionEvent)
+    {
+        System.out.println("track is moved downwards");
+    }
 }

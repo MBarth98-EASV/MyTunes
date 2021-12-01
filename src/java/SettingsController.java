e43ab00e8a3ee822df24c467f19380502e4d6646
sandbxk <90904706+sandbxk@users.noreply.github.com>
2021-12-01 11:05:20 +0100

    Added directoryChooser to settings

--- src/java/SettingsController.java
+++ src/java/SettingsController.java
@@ -3,9 +3,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.util.ResourceBundle;
 
 public class SettingsController implements Initializable {
@@ -25,8 +30,21 @@ public class SettingsController implements Initializable {
 
     }
 
+
+    /**
+     * Opens a DirectoryChooser, and sets the accompanying TextField's text as the directory.
+     * Should the user choose to manipulate the chosen directory, it parses the text in the TextField
+     * as a path, which is then used by other classes.
+     */
     @FXML
     public void onSelectDirectory(ActionEvent event){
+        DirectoryChooser dc = new DirectoryChooser();
+
+        File selectedDirectory = dc.showDialog(new Stage());
+        txtFieldDirectory.setText(selectedDirectory.getAbsolutePath());
+
+
+        Path path = Path.of(txtFieldDirectory.getText());
 
     }
 

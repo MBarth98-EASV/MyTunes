993a42ee45486f821c43f1df1146456cd6423a1d
Mads Mandahl-Barth <mads-barth@live.dk>
2021-12-01 20:22:42 +0100

    format for song controller

--- src/java/SongController.java
+++ src/java/SongController.java
@@ -1,24 +1,22 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-
 import java.io.File;
 import java.net.URL;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.ResourceBundle;
 
-public class SongController implements Initializable {
+public class SongController implements Initializable
+{
+    @FXML
+    public TextField txtFieldSongPath;
 
-    @FXML public TextField txtFieldSongPath;
-
-    @FXML public Button btnSelectFile;
+    @FXML
+    public Button btnSelectFile;
 
     String songPath = null;
 
@@ -28,24 +26,23 @@ public class SongController implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources)
+    {
 
     }
 
-    public void onSelectFile(ActionEvent event) {
+    public void onSelectFile(ActionEvent event)
+    {
         FileChooser fc = new FileChooser();
 
-        File selectedFile =  fc.showOpenDialog(new Stage());
+        File selectedFile = fc.showOpenDialog(new Stage());
         txtFieldSongPath.setText(selectedFile.getAbsolutePath());
 
-
         songPath = txtFieldSongPath.getText();
-
     }
 
-    public void onAddSong(ActionEvent event) {
-        ((Node)(event.getSource())).getScene().getWindow().hide();
+    public void onAddSong(ActionEvent event)
+    {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
     }
-
-
 }

0b376fc0a3c3852d97c60fe01ba3cae09b484524
Mads Mandahl-Barth <mads-barth@live.dk>
2021-12-01 20:21:53 +0100

    format settingscontroller

--- src/java/SettingsController.java
+++ src/java/SettingsController.java
@@ -1,23 +1,22 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-
-
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.ResourceBundle;
 
-public class SettingsController implements Initializable {
+public class SettingsController implements Initializable
+{
+    @FXML
+    public TextField txtFieldDirectory;
 
-    @FXML public TextField txtFieldDirectory;
-
-    @FXML public Button btnSelectDirectory;
-
+    @FXML
+    public Button btnSelectDirectory;
 
     public SettingsController()
     {
@@ -25,10 +24,10 @@ public class SettingsController implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources)
+    {
 
     }
-
 
     /**
      * Opens a DirectoryChooser, and sets the accompanying TextField's text as the directory.
@@ -36,15 +35,13 @@ public class SettingsController implements Initializable {
      * as a path, which is then used by other classes.
      */
     @FXML
-    public void onSelectDirectory(ActionEvent event){
+    public void onSelectDirectory(ActionEvent event)
+    {
         DirectoryChooser dc = new DirectoryChooser();
 
         File selectedDirectory = dc.showDialog(new Stage());
         txtFieldDirectory.setText(selectedDirectory.getAbsolutePath());
 
-
         Path path = Path.of(txtFieldDirectory.getText());
-
     }
-
 }

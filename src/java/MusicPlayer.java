ad4a5efe98b9e0822bbd2e01e8e15a48a3f7b1d2
Mads Mandahl-Barth <mads-barth@live.dk>
2021-12-01 20:20:56 +0100

    format for musicplayer

--- src/java/MusicPlayer.java
+++ src/java/MusicPlayer.java
@@ -2,35 +2,35 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import java.io.File;
 
-public class MusicPlayer {
+public class MusicPlayer
+{
+    File filepath = new File(Controller.class.getResource("music/file_example_MP3_5MG.mp3").getFile());
 
-        File filepath = new File(Controller.class.getResource("music/file_example_MP3_5MG.mp3").getFile());
+    Media media = new Media(filepath.toURI().toString());
+    MediaPlayer musicPlayer = new MediaPlayer(media);
 
-        Media media = new Media(filepath.toURI().toString());
-        MediaPlayer musicPlayer = new MediaPlayer(media);
+    public void playTrack()
+    {
+        musicPlayer.play();
+    }
 
-        public void playTrack()
-        {
-            musicPlayer.play();
-        }
+    public void pauseTrack()
+    {
+        musicPlayer.pause();
+    }
 
-        public void pauseTrack()
-        {
-            musicPlayer.pause();
-        }
+    public void stopTrack()
+    {
 
-        public void stopTrack()
-        {
+    }
 
-        }
+    public void nextTrack()
+    {
 
-        public void nextTrack()
-        {
+    }
 
-        }
+    public void previousTrack()
+    {
 
-        public void previousTrack()
-        {
-
-        }
+    }
 }

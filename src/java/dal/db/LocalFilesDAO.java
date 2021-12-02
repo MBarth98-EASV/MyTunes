0d84e444ff175f223e9e6137507f89f88ab86370
sandbxk <90904706+sandbxk@users.noreply.github.com>
2021-12-02 21:51:27 +0100

    Somewhat close to loading all local files. Need to figure out lib and songModel

--- src/java/dal/db/LocalFilesDAO.java
+++ src/java/dal/db/LocalFilesDAO.java
@@ -1,15 +1,13 @@
 package dal.db;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
+import be.SongModel;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
@@ -24,17 +22,16 @@ public class LocalFilesDAO {
 
     private Path currentPath = null;
 
-    private static final Path songPath = Path.of("data/externalsongs.txt");
+    private static final Path songPath = Path.of("src/resources/data/externalsongs.txt");
     private static final Path dirPath = Path.of("data/directory.txt");
 
 
     /**
-     *
      * @param path The given Path to read files from.
      * @return Every file in the path's directory and subdirectories.
      */
 
-    public List<File> readAllFromNewDir(Path path){
+    public List<File> readAllFromNewDir(Path path) {
         saveDirectory(path);
         ArrayList<File> returnList = new ArrayList<>();
 
@@ -44,12 +41,31 @@ public class LocalFilesDAO {
         ArrayList<File> allLocalFilePaths = new ArrayList<>();
         allLocalFilePaths.addAll(List.of(listOfFiles));
 
-        for (File f : allLocalFilePaths){
+        for (File f : allLocalFilePaths) {
 
-            if(checkForMp3OrWav(f.getName())) {
+            if (checkForMp3OrWav(f.getName())) {
                 returnList.add(f);
+            } else if (f.isDirectory()) {
+                readAllFromNewDir(Path.of(f.getPath()));
             }
-            else if (f.isDirectory()){
+        }
+        return returnList;
+    }
+
+    public List<Path> readAllFromCurDirectory() {
+        ArrayList<Path> returnList = new ArrayList<>();
+
+        File filePath = currentPath.toFile();
+        File[] listOfFiles = filePath.listFiles();
+
+        ArrayList<File> allLocalFilePaths = new ArrayList<>();
+        allLocalFilePaths.addAll(List.of(listOfFiles));
+
+        for (File f : allLocalFilePaths) {
+
+            if (checkForMp3OrWav(f.getName())) {
+                returnList.add(f.toPath());
+            } else if (f.isDirectory()) {
                 readAllFromNewDir(Path.of(f.getPath()));
             }
         }
@@ -57,7 +73,7 @@ public class LocalFilesDAO {
     }
 
 
-    private void saveDirectory(Path path){
+    private void saveDirectory(Path path) {
         try (BufferedWriter bw = Files.newBufferedWriter(dirPath, StandardOpenOption.SYNC,
                 StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE)) {
 
@@ -73,16 +89,15 @@ public class LocalFilesDAO {
         }
     }
 
-    public Path addSong(Path path){
+    public Path addSong(Path path) {
 
         try (BufferedWriter bw = Files.newBufferedWriter(songPath, StandardOpenOption.SYNC,
                 StandardOpenOption.APPEND, StandardOpenOption.WRITE)) {
 
-            if (checkForMp3OrWav(String.valueOf(path))){
+            if (checkForMp3OrWav(String.valueOf(path))) {
                 bw.newLine();
                 bw.write(String.valueOf(path));
-            }
-            else throw new Exception("The file you added is not currently supported");
+            } else throw new Exception("The file you added is not currently supported");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -95,26 +110,91 @@ public class LocalFilesDAO {
         return path;
     }
 
-    public List<Path> loadAllExternalSongs(){
+    public List<Path> loadAllExternalSongs() {
         ArrayList<Path> returnList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(String.valueOf(songPath)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
 
-        
+                String songPath = line;
+                returnList.add(Path.of(songPath));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return returnList;
+    }
+
+    public void removeSong(SongModel song) {
+
     }
 
 
-    private Boolean checkForMp3OrWav(String fileName){
+    private Boolean checkForMp3OrWav(String fileName) {
         String extension = "";
 
         int i = fileName.lastIndexOf('.');
         if (i > 0) {
-            extension = fileName.substring(i+1);
+            extension = fileName.substring(i + 1);
         }
-        if(extension.equals("mp3") || extension.equals("wav")){
+        if (extension.equals("mp3") || extension.equals("wav")) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
+    private List<SongModel> loadAllLocalSongs() {
+        ArrayList<Path> loadList = new ArrayList<>();
+        loadList.addAll(readAllFromCurDirectory());
+        loadList.addAll(loadAllExternalSongs());
+
+        ArrayList<SongModel> returnList = new ArrayList<>();
+
+        try {
+            for (Path p : loadList) {
+                //String fileLocation = String.valueOf(p.toAbsolutePath());
 
 
+                InputStream input = new FileInputStream(p.toFile());
+                ContentHandler handler = new DefaultHandler();
+                Metadata metadata = new Metadata();
+                Parser parser = new Mp3Parser();
+                ParseContext parseCtx = new ParseContext();
+                parser.parse(input, handler, metadata, parseCtx);
+                input.close();
+
+                //returnList.add(new SongModel(null, metadata.get("title"), metadata.get("xmpDM:artist"), null, null, p));
+
+                // List all metadata
+                String[] metadataNames = metadata.names();
+
+                for (String name : metadataNames) {
+                    System.out.println(name + ": " + metadata.get(name));
+                }
+            }
+            // Retrieve the necessary info from metadata
+  /*              // Names - title, xmpDM:artist etc. - mentioned below may differ based
+                System.out.println("----------------------------------------------");
+                System.out.println("Title: " + metadata.get("title"));
+                System.out.println("Artists: " + metadata.get("xmpDM:artist"));
+                System.out.println("Composer : " + metadata.get("xmpDM:composer"));
+                System.out.println("Genre : " + metadata.get("xmpDM:genre"));
+                System.out.println("Album : " + metadata.get("xmpDM:album"));
+*/
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TikaException e) {
+        }
+
+
+        return returnList;
+    }
 }
+

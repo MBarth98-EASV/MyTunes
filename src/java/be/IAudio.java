962484ed1453e715375d5e7a6490ec065863eb76
Mads Mandahl-Barth <mads-barth@live.dk>
2021-12-06 18:30:30 +0100

    Create IAudio.java

---
+++ src/java/be/IAudio.java
@@ -0,0 +1,17 @@
+package be;
+
+import be.SongModel;
+
+public interface IAudio
+{
+    void play();
+    void pause();
+    void reset();
+    void skipTo(int ms);
+
+    void load(SongModel song);
+    void load(String path);
+
+    int getDuration(); // ms
+    int getTime(); // ms offset
+}

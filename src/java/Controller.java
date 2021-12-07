e65cb6adb90f77ab9517495b1fb794d7fd18c75f
Mads Mandahl-Barth <mads-barth@live.dk>
2021-12-07 15:24:33 +0100

    added aux file to store the fxml properties to make the controller less cluttered

--- src/java/Controller.java
+++ src/java/Controller.java
@@ -1,3 +1,4 @@
+import be.MyTunesFXMLProperties;
 import be.SongModel;
 import dal.db.EASVDatabase;
 import javafx.beans.property.SimpleListProperty;
@@ -25,74 +26,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable
+public class Controller extends MyTunesFXMLProperties implements Initializable
 {
-    @FXML public TextField txtFieldSearch;
 
-    @FXML public Button btnSettings;
-
-    @FXML public TreeTableView treeView;
-
-    @FXML public TreeTableColumn tvColumnPlaylist;
-
-    @FXML public TreeTableColumn tvColumnArtist;
-
-    @FXML public TreeTableColumn tvColumnSongs;
-
-    @FXML public TreeTableColumn tvColumnTime;
-
-    @FXML public Button btnPlaylistUp;
-
-    @FXML public Button btnPlaylistDown;
-
-    @FXML public Button btnPlaylistNew;
-
-    @FXML public Button btnPlaylistEdit;
-
-    @FXML public Button btnPlaylistDelete;
-
-    @FXML public TableView<SongModel> tblViewSongs = new TableView<SongModel>();
-
-    @FXML public TableColumn<SongModel, String> tblClmnSongTitle = new TableColumn<SongModel, String>();
-
-    @FXML public TableColumn<SongModel, String> tblClmnSongArtist = new TableColumn<SongModel, String>();
-
-    @FXML public TableColumn<SongModel, String> tblClmnSongAlbum = new TableColumn<SongModel, String>();
-
-    @FXML public TableColumn<SongModel, String> tblClmnSongGenre = new TableColumn<SongModel, String>();
-
-    @FXML public TableColumn<SongModel, String> tblClmnSongTime = new TableColumn<SongModel, String>();
-
-    @FXML public Button btnSongUp;
-
-    @FXML public Button btnSongDown;
-
-    @FXML public Button btnSongNew;
-
-    @FXML public Button btnSongEdit;
-
-    @FXML public Button btnSongDelete;
-
-    @FXML public Button btnPlayPause;
-
-    @FXML public Button btnNextSong;
-
-    @FXML public Button btnPrevSong;
-
-    @FXML public Label lblSongName;
-
-    @FXML public Label lblSongCurrentTime;
-
-    @FXML public Label lblSongTotalTime;
-
-    @FXML public Slider sliderSong;
-
-    @FXML public ToggleButton tglBtnShuffle;
-
-    @FXML public Slider sliderVolume;
-
-
-    private static boolean isPlaying = true;
 
     final ObservableList<SongModel> data = FXCollections.observableArrayList();
 

532f691ea7c16b7239ea96a34c7c3aec3e8b3ac0
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2021-12-08 16:47:42 +0100

    Updated code to make it work

--- src/java/dal/db/EASVDatabase.java
+++ src/java/dal/db/EASVDatabase.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 
 public class EASVDatabase 
 {
